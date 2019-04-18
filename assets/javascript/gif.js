70451dc6d6c794e33da15a3aa64f7ae0b5435c94
The Becs <beccyvalentine@gmail.com>
2019-04-17 19:01:14 -0700

    added existing folders to repo

---
+++ assets/javascript/gif.js
@@ -0,0 +1,71 @@
+// Empty array of sports teams
+var sportsTeam =[];
+
+
+// Function for displaying sports team data
+function renderButtons() {
+
+    // Deleting the buttons if any prior to addind new ones
+    // (this is necessary otherwise we will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of teams
+    for (var i = 0; i < sportsTeam.length; i++) {
+
+      // Then dynamicaly generating buttons for each movie in the array
+      var a = $("<button>");
+      // Adding a class
+      a.addClass("team");
+      // Added a data-attribute
+      a.attr("data-name", sportsTeam[i]);
+      // Provided the initial button text
+      a.text(sportsTeam[i]);
+      // Added the button to the HTML
+      $("#buttons-view").append(a);
+    }
+  }
+
+   // This function handles events where one button is clicked
+   $("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var team = $("#sport-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    sportsTeam.push(team);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  });
+
+  renderButtons();
+
+  $(".team").on("click", function() {
+    var sportsteam = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sportsteam + "&api_key=3QGN2O8Bws9dO6cv6z5FmzS3twWYL4ZZ&limit=10&offset=0&rating=PG-13&lang=en"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var teamImage = $("<img>");
+          teamImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(teamImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+      });
+  });
